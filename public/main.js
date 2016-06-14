a43bb73a278a9e1b1614fecb81258ea8c395e316
Kelsea <andersonkelsea@gmail.com>
2016-06-14 19:51:32 -0400

    Added mouseup/mousedown drawing

--- public/main.js
+++ public/main.js
@@ -1,10 +1,12 @@
+var drawing = false;
+
 var pictionary = function() {
     var canvas, context;
 
     var draw = function(position) {
         context.beginPath();
         context.arc(position.x, position.y,
-                         6, 0, 2 * Math.PI);
+           6, 0, 2 * Math.PI);
         context.fill();
     };
 
@@ -12,12 +14,23 @@ var pictionary = function() {
     context = canvas[0].getContext('2d');
     canvas[0].width = canvas[0].offsetWidth;
     canvas[0].height = canvas[0].offsetHeight;
-    canvas.on('mousemove', function(event) {
-        var offset = canvas.offset();
-        var position = {x: event.pageX - offset.left,
-                        y: event.pageY - offset.top};
-        draw(position);
+
+    canvas.on("mousedown", function(event){
+        drawing = true;
     });
+
+    canvas.on("mouseup", function(event){
+        drawing = false;
+    });
+
+    canvas.on('mousemove', function(event) {
+        if (drawing){
+            var offset = canvas.offset();
+            var position = {x: event.pageX - offset.left,
+                            y: event.pageY - offset.top};
+            draw(position);
+            }
+        });
 };
 
 $(document).ready(function() {
